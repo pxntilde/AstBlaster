931c6295246851d4063b815c069a4840f75b2bf6
Andrew <pXn.Tilde@protonmail.com>
2022-08-24 21:50:12 -0700

    Game singleton

--- AstBlaster/Game.cs
+++ AstBlaster/Game.cs
@@ -8,6 +8,43 @@ namespace AstBlaster
     /// </summary>
     public class Game : Node
     {
-        
+        /// <summary>
+        /// Default place area size
+        /// </summary>
+        private static readonly Vector2 DefaultSize = new Vector2(1920f, 1080f);
+
+        /// <summary>
+        /// Game instance
+        /// </summary>
+        public static Game Instance;
+
+        /// <summary>
+        /// Play bounds
+        /// </summary>
+        [Export]
+        public Rect2 Bounds { get; private set; } = new(-1f * DefaultSize / 2f, DefaultSize);
+
+        /// <summary>
+        /// ENTER TREE
+        /// </summary>
+        public override void _EnterTree()
+        {
+            if (Instance is not null)
+            {
+                GD.PrintErr($"{this} {Name} :: Tried to add second {nameof(Game)} instance");
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        /// <summary>
+        /// EXIT TREE
+        /// </summary>
+        public override void _ExitTree()
+        {
+            Instance = null;
+        }
     }
 }
