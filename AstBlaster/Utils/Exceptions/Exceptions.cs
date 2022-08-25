56abcbb31fb98424662299a85223e8a1191cfd66
Andrew <pXn.Tilde@protonmail.com>
2022-08-24 21:50:00 -0700

    Exception/warning

---
+++ AstBlaster/Utils/Exceptions/Exceptions.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstBlaster.Utils.Exceptions
+{
+    public class Error : Exception
+    {
+        public Error(String message) : base(message)
+        {
+            GD.PrintErr(message);
+        }
+    }
+
+    /// <summary>
+    /// Error when argument is out of the allowable range by the function
+    /// </summary>
+    public class ArgumentOutOfRangeError : ArgumentOutOfRangeException
+    {
+        public ArgumentOutOfRangeError(String argument) : base(argument)
+        {
+            GD.PrintErr(argument);
+        }
+
+        public ArgumentOutOfRangeError(String argument, String message) : base(argument, message)
+        {
+            GD.PrintErr($"{argument} {message}");
+        }
+    }
+}
---
+++ AstBlaster/Utils/Exceptions/Warning.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+
+namespace AstBlaster.Utils.Exceptions
+{
+    /// <summary>
+    /// Static warning functions
+    /// </summary>
+    public static class Warning
+    {
+        /// <summary>
+        /// Prints a warning message to the console
+        /// </summary>
+        /// <param name="message">Message to print</param>
+        public static void Print(String message)
+        {
+            GD.PushWarning(message);
+        }
+
+        /// <summary>
+        /// Prints a warning message to the console
+        /// </summary>
+        /// <param name="node">Node emitting the warning</param>
+        /// <param name="message">Message to print</param>
+        public static void Print(Node node, String message)
+        {
+            GD.PushWarning($"{node} :: {message}");
+        }
+    }
+}
